13614ee85aa3c55581e964cc2f6f13c474640820
kn991 <kn911@yandex.ru>
2025-08-22 17:49:48 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        CarCollection cars = new CarArrayList();
+        for (int i = 0; i < 10; i++) {
+            cars.add(new Car("Brand" + i, i));
+        }
+
+        for (Car car : cars) {
+            System.out.println(car.getBrand() + " " + car.getNumber());
+        }
+    }
+}
---
+++ src/test/java/CarCollectionTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarCollectionTest {
+
+    private CarCollection carCollection;
+
+    @BeforeEach
+    void setUp() {
+        carCollection = new CarHashSet();
+        for (int i = 0; i < 100; i++) {
+            carCollection.add(new Car("Brand" + i, i));
+        }
+    }
+
+    @Test
+    public void contains() {
+        assertTrue(carCollection.contains(new Car("Brand20", 20)));
+        assertFalse(carCollection.contains(new Car("fuck", 22222)));
+    }
+
+    @Test
+    public void testForEach() {
+        int index = 0;
+        for (Car car : carCollection) {
+            index++;
+        }
+
+        assertEquals(100, index);
+    }
+}
